d063a96a4f00236cea90e17e98b3d66c9bc225c0
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 19:24:59 -0300

    feat: :zap: Adicionando mensagens personalizadas ao schema

--- src/middleware/schemas.js
+++ src/middleware/schemas.js
@@ -3,6 +3,11 @@ const joi = require('joi');
 const schemaLogin = joi.object({
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
+}).messages({
+  'any.required': 'Some required fields are missing',
+  'string.empty': 'Some required fields are missing',
+  'string.email': 'Some required fields are missing',
+  'string.min': 'Some required fields are missing',
 });
 
 const schemaUser = joi.object({
