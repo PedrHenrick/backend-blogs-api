f058160c50a70f64ca16492771579f193aa67850
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 14:35:39 -0300

    feat: :sparkles: Criando migration da tabela PostCategories

---
+++ src/database/migrations/20220623171803-create-post-categorie.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
