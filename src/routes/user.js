2cf62d0bf2e6bd9a3f181f71daa2da53df18fa49
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 19:26:49 -0300

    feat: :sparkles: Criando método GET no endpoint /user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/user.controller.js');
+const authenticateMiddleware = require('../middleware/authenticateMiddleware.js');
 const validateSchema = require('../middleware/middlewareValidate');
 const { schemaUser } = require('../middleware/schemas');
 
 const userRouter = express.Router();
 
-userRouter.post(
-  '/',
-  validateSchema(schemaUser),
-  userController.add,
-);
+userRouter.get('/', authenticateMiddleware, userController.getAll);
+userRouter.post('/', validateSchema(schemaUser), userController.add);
 
 module.exports = userRouter;
