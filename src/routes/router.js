73462dc3144bd72c8c15ad4e49ae78482bc8d06d
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 17:53:24 -0300

    feat: :sparkles: Criando rota user no index de routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/login', require('./login'));
+router.use('/user', require('./user'));
 
 module.exports = router;
