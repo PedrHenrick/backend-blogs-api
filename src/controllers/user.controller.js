9bc745261673c14835fabf2d87ff7cd15962cf44
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 19:27:52 -0300

    feat: :sparkles: Criando função getall no controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,13 @@
 const userService = require('../services/user.service');
 
+const getAll = async (_req, res) => {
+  const user = await userService.getAll();
+  res.status(200).json(user);
+};
+
 const add = async (req, res) => {
   const user = await userService.add(req.body);
   res.status(201).json(user);
 };
 
-module.exports = {
-  add,
-};
+module.exports = { add, getAll };
