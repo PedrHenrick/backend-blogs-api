ab177bd34dfd2350625f914af806b7d01963930f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 19:27:25 -0300

    feat: :sparkles: Criando middleware de validação de autorização

---
+++ src/middleware/authenticateMiddleware.js
@@ -0,0 +1,14 @@
+const { authenticateToken } = require('../utils/JWTToken');
+
+const errorObjectEmptyToken = { status: 401, message: 'Token not found' };
+
+const authenticateMiddleware = async (req, _res, next) => {
+  const token = req.headers.authorization;
+  if (!token) throw errorObjectEmptyToken;
+  
+  const user = await authenticateToken(token);
+  console.log(user);
+  next();
+};
+
+module.exports = authenticateMiddleware;
