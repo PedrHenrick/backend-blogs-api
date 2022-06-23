bcf48d20f56779fd0243d2c2fabe18309b078b81
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-23 17:54:55 -0300

    feat: :sparkles: Criando middleware de validação genérico

---
+++ src/middleware/middlewareValidate.js
@@ -0,0 +1,19 @@
+const returnArray = (object) => {
+  if(object[0]) return object;
+  return [object];
+}
+
+const validateSchema = (schema) => (req, res, next) => {
+  const itemsForVerification = returnArray(req.body);
+
+  itemsForVerification.forEach((item) => {
+    const { error } = schema.validate(item, { abortEarly: false });
+    if (error) {
+      const messages = error.details[0].message;
+      return res.status(400).json({ message: messages });
+    }
+  });
+  next();
+};
+
+module.exports = validateSchema;
