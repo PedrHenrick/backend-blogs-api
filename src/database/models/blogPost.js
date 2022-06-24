4912cad2b23faabc4282ddb47812d0ed345fbd30
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-24 17:55:41 -0300

    feat: :sparkles: Criando model de blogPost e adicionando associação

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,29 @@
+const blogPostSchema = (sequelize, DataTypes) => {
+  const blogPostTable = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  }, { timestamps: false });
+
+  blogPostTable.associate = models => {
+    blogPostTable.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user'
+    });
+  };
+
+  return blogPostTable;
+};
+
+
+module.exports = blogPostSchema;
