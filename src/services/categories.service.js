4dcec42e10700c5d61caafa57706826a183f700e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-24 17:32:28 -0300

    feat: :sparkles: Criando função getAll no service da rota /categories

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -2,6 +2,11 @@ const { Category } = require('../database/models');
 
 const errorObjectCategoryExist = { status: 400, message: 'Category exists' };
 
+const getAll = async () => {
+  const allCategories = await Category.findAll({ order: [['id', 'ASC']] });
+  return allCategories;
+};
+
 const add = async (categoryInformations) => {
   try {
     const hasCategoryAdded = await Category.create(categoryInformations);
@@ -11,4 +16,4 @@ const add = async (categoryInformations) => {
   }
 };
 
-module.exports = { add }; 
+module.exports = { getAll, add }; 
