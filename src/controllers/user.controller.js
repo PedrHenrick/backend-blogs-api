0ba04e2bf99289a12f66361f30f1e24dfa7adb18
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-24 15:11:31 -0300

    feat: :sparkles: Criando função GetById no controller de /user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,9 +5,14 @@ const getAll = async (_req, res) => {
   res.status(200).json(user);
 };
 
+const getById = async (req, res) => {
+  const userWithId = await userService.getById(req.params);
+  res.status(200).json(userWithId);
+};
+
 const add = async (req, res) => {
   const user = await userService.add(req.body);
   res.status(201).json(user);
 };
 
-module.exports = { add, getAll };
+module.exports = { getAll, getById, add };
