6224019457d1a26250b90cd4c7e83f754a96e935
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-25 13:45:47 -0300

    feat: :sparkles: Criando rota /post

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.use('/login', require('./login'));
 router.use('/user', require('./user'));
 router.use('/categories', require('./categories'));
+router.use('/post', require('./post'));
 
 module.exports = router;
