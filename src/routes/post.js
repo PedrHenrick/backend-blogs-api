6bb8ceb15d0eb13cbc0042d9e77387a884ccc769
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-25 13:46:25 -0300

    feat: :sparkles: Adicionando método POST na rota /post
    Adicionando middlewares de validação e autenticação

---
+++ src/routes/post.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const postController = require('../controllers/post.controller');
+const authenticateMiddleware = require('../middleware/authenticateMiddleware');
+const validateSchema = require('../middleware/middlewareValidate');
+const { schemaPost } = require('../middleware/schemas');
+
+const postRouter = express.Router();
+
+postRouter.post(
+  '/',
+  authenticateMiddleware,
+  validateSchema(schemaPost),
+  postController.add,
+);
+
+module.exports = postRouter;
